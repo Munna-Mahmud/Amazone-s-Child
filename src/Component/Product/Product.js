a1aa12aa5eed8d80c65d7e4a437bc9084302ad38
Munna-Mahmud <learnermunna@gmail.com>
2021-09-24 21:35:19 +0600

    site ready

--- src/Component/Product/Product.js
+++ src/Component/Product/Product.js
@@ -1,24 +1,31 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import {faShoppingCart } from '@fortawesome/free-solid-svg-icons'
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 
 import './Product.css'
+import Rating from 'react-rating';
 
 const Product = (props) => {
-    const {name, img, seller, price, stock } =props.product
-    const cartIcons = <FontAwesomeIcon icon={faShoppingCart}/>
+    const { name, img, seller, price, stock, star } = props.product
+    const cartIcons = <FontAwesomeIcon icon={faShoppingCart} />
     return (
-        <div className ="prouct">
+        <div className="prouct">
             <div>
-            <img src={img} alt="" />
+                <img src={img} alt="" />
             </div>
-           <div>
-           <h4 className="product-name">{name}</h4>
-            <p><small>By: {seller}</small></p>
-            <p>Price :{price}</p>
-            <p><small>Only {stock} left in stock -order soon</small></p>
-            <button onClick ={() => props.handleAddToCart (props.product)}  className="purchase-btn"> {cartIcons}add to cart</button>
-           </div>
+            <div>
+                <h4 className="product-name">{name}</h4>
+                <p><small>By: {seller}</small></p>
+                <p>Price :{price}</p>
+                <Rating className="rating"
+                    initialRating={star}
+                    emptySymbol="far fa-star icon-color"
+                    fullSymbol="fas fa-star icon-color"
+                    readonly> </Rating>
+                <p><small>Only {stock} left in stock -order soon</small></p>
+               
+                <button onClick={() => props.handleAddToCart(props.product)} className="purchase-btn"> {cartIcons}add to cart</button>
+            </div>
         </div>
     );
 };
